c10157c88b50fe5337d481b2861a50e26602aadd
Boofdev <hexahigh0@gmail.com>
2025-11-22 15:14:58 +0100

    Update lenis

--- src/scripts/smoothScrollingEverywhere.user.ts
+++ src/scripts/smoothScrollingEverywhere.user.ts
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://*/*
 // @grant       none
-// @version     2.0
+// @version     2.1
 // @author      Boofdev
 // @description Universal smooth scrolling for (almost) all websites using Lenis
 // @updateURL   https://us.080609.xyz/scripts/smoothScrollingEverywhere.user.js
@@ -21,6 +21,8 @@ import { addGlobalCSS } from '../util';
   // Initialize Lenis smooth scrolling
   const lenis = new Lenis({
     autoRaf: true,
+    autoToggle: true, // Doesn't work on older browsers
+    allowNestedScroll: true, // Can cause performance issues, but is essentially needed since we are trying to use Lenis everywhere
     prevent: (node) =>
       node.classList.contains('no-lenis-scroll') ||
       node.classList.contains('no-smooth-scroll') ||
